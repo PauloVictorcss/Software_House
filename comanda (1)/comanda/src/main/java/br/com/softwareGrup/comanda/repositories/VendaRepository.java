c76c7006532b93935f57544f5ddf61d008902c70
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2024-06-12 20:38:09 -0300

    Update VendaRepository.java

--- comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/repositories/VendaRepository.java
+++ comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/repositories/VendaRepository.java
@@ -3,7 +3,12 @@ package br.com.softwareGrup.comanda.repositories;
 import br.com.softwareGrup.comanda.model.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface VendaRepository extends JpaRepository<Venda, Long> {
+
+    List<Venda> findByDataHoraPagamentoBetween(Date inicio, Date fim);
+
 }
