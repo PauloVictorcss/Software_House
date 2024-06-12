bc96e08501c6a2bc3e441d69730f475afbcd6e9e
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2024-06-12 20:40:16 -0300

    Update MesaServicies.java

--- comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/servicies/MesaServicies.java
+++ comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/servicies/MesaServicies.java
@@ -1,14 +1,20 @@
 package br.com.softwareGrup.comanda.servicies;
 
+import br.com.softwareGrup.comanda.enuns.FormaPagamento;
 import br.com.softwareGrup.comanda.enuns.StatusMesa;
 import br.com.softwareGrup.comanda.exception.EntityNotFoundException;
 import br.com.softwareGrup.comanda.exception.IsNotPossibleToDoException;
-import br.com.softwareGrup.comanda.model.Mesa;
+import br.com.softwareGrup.comanda.model.*;
+import br.com.softwareGrup.comanda.repositories.ComandaRepository;
 import br.com.softwareGrup.comanda.repositories.MesaRepository;
+import br.com.softwareGrup.comanda.repositories.ProdutoRepository;
+import br.com.softwareGrup.comanda.repositories.VendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,44 +22,66 @@ public class MesaServicies {
 
     @Autowired
     MesaRepository repository;
+    @Autowired
+    ComandaRepository comandaRepository;
+    @Autowired
+    VendaRepository vendaRepository;
+    @Autowired
+    ProdutoRepository produtoRepository;
 
     public List<Mesa> getMesas(){
         return repository.findAll();
 
     }
 
-    public Mesa abrirMesa(Long id, int numeroOcupantes) {
-        Mesa mesa = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Mesa não existe"));
-        if(mesa.getStatus().equals(StatusMesa.LIVRE)) {
-            mesa.setNumeroOcupante(numeroOcupantes);
-            mesa.setHoraAbertura(LocalDateTime.now());
-            mesa.setHoraFechamento(null);
-            mesa.setStatus(StatusMesa.OCUPADA);
-            return repository.save(mesa);
+    public List<Produto> abrirMesa(Mesa mesa) {
+        Mesa mesaNoBanco = repository.findById(mesa.getId()).orElseThrow(() -> new EntityNotFoundException("Mesa não existe"));
+        if(mesaNoBanco.getStatus().equals(StatusMesa.LIVRE)) {
+            mesaNoBanco.setNumeroOcupante(mesa.getNumeroOcupante());
+            mesaNoBanco.setHoraAbertura(LocalDateTime.now());
+            mesaNoBanco.setHoraFechamento(null);
+            mesaNoBanco.setStatus(StatusMesa.OCUPADA);
+            repository.save(mesaNoBanco);
+            return produtoRepository.findAll();
         } else {
             throw new IsNotPossibleToDoException("Não é possível fazer abertura pois a mesa não está livre");
         }
     }
 
-    public Mesa AguardarPagamento(Long idMesa) {
-        Mesa mesa = repository.findById(idMesa).orElseThrow(() -> new EntityNotFoundException("Mesa não existe"));
-        if(mesa.getHoraAbertura() == null){
+    public Mesa AguardarPagamento(Mesa mesa) {
+        Mesa mesaNoBanco = repository.findById(mesa.getId()).orElseThrow(() -> new EntityNotFoundException("Mesa não existe"));
+        if(mesaNoBanco.getHoraAbertura() == null){
             throw new IsNotPossibleToDoException("Se a mesa não foi aberta não é possível aguardar pagamento");
         }else {
-            mesa.setStatus(StatusMesa.AGUARDANDO_PAGAMENTO);
-            return repository.save(mesa);
+            mesaNoBanco.setStatus(StatusMesa.AGUARDANDO_PAGAMENTO);
+            return repository.save(mesaNoBanco);
         }
     }
 
-    public Mesa fechaMesa(Long idMesa, int numeroOcupantes) {
-        Mesa mesa = repository.findById(idMesa).orElseThrow(() -> new EntityNotFoundException("Mesa não existe"));
-        if(mesa.getHoraAbertura() == null){
+    @Transactional
+    public Venda fechaMesa(Mesa mesa, FormaPagamento formaPagamento) {
+        Mesa mesaNoBanco = repository.findById(mesa.getId()).orElseThrow(() -> new EntityNotFoundException("Mesa não existe"));
+        if(mesaNoBanco.getHoraAbertura() == null){
             throw new IsNotPossibleToDoException("Não é possível fechar mesa sem antes ter abrido");
         } else {
-            mesa.setNumeroOcupante(numeroOcupantes);
-            mesa.setHoraFechamento(LocalDateTime.now());
-            mesa.setStatus(StatusMesa.LIVRE);
-            return repository.save(mesa);
+
+            Comanda comada = comandaRepository.findByMesa(mesa).orElse(null);
+            List<ItemPedido> itens = comada.getItens();
+            double subTotal = itens.stream().mapToDouble(item -> item.getProduto().getPreco() * item.getQuantidade()).sum();
+
+            Venda venda = new Venda();
+            venda.setMesa(mesaNoBanco);
+            venda.setComanda(comada);
+            venda.setSubtotal(subTotal);
+            venda.setDataHoraPagamento(new Date());
+            venda.setFormaPagamento(formaPagamento);
+
+            mesaNoBanco.setNumeroOcupante(mesa.getNumeroOcupante());
+            mesaNoBanco.setHoraFechamento(LocalDateTime.now());
+            mesaNoBanco.setStatus(StatusMesa.LIVRE);
+
+            repository.save(mesaNoBanco);
+            return vendaRepository.save(venda);
         }
     }
 
