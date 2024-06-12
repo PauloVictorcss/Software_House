4ff8e7f367d41d5d5af8cb33c3f5bd6f58029a86
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2024-06-12 20:17:15 -0300

    Update MesaController.java

--- comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/controllers/MesaController.java
+++ comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/controllers/MesaController.java
@@ -1,6 +1,9 @@
 package br.com.softwareGrup.comanda.controllers;
 
+import br.com.softwareGrup.comanda.enuns.FormaPagamento;
 import br.com.softwareGrup.comanda.model.Mesa;
+import br.com.softwareGrup.comanda.model.Produto;
+import br.com.softwareGrup.comanda.model.Venda;
 import br.com.softwareGrup.comanda.servicies.MesaServicies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,22 +22,24 @@ public class MesaController {
         return mesaServicies.getMesas();
     }
 
-    @PutMapping("/abrir")
-    public Mesa abrirMesa(@RequestParam Long idMesa, @RequestParam int numeroOcupantes) {
-        return mesaServicies.abrirMesa(idMesa, numeroOcupantes);
+    @PostMapping("/abrir")
+    public List<Produto> abrirMesa(@RequestBody Mesa mesa) {
+        return mesaServicies.abrirMesa(mesa);
     }
 
     @PutMapping("/aguardar")
-    public Mesa aguardarPagamento(@RequestParam Long idMesa) {
-       return mesaServicies.AguardarPagamento(idMesa);
+    public Mesa aguardarPagamento(@RequestBody Mesa mesa) {
+       return mesaServicies.AguardarPagamento(mesa);
     }
+
 
     @PutMapping("/fechar")
-    public Mesa fecharMesa(@RequestParam Long idMesa, @RequestParam int numeroOcupantes) {
-       return mesaServicies.fechaMesa(idMesa, numeroOcupantes);
+    public Venda fecharMesa(@RequestBody Mesa mesa, @RequestParam FormaPagamento formaPagamento) {
+        return mesaServicies.fechaMesa(mesa, formaPagamento);
     }
 
-    @PostMapping
+
+    @PostMapping("/popular")
     public void populaMesa(@RequestBody Mesa mesa){
         mesaServicies.createMesa(mesa);
     }
