4857542abf4f2df1a6ad4e033fe6313f55833e1c
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2024-06-13 20:18:13 -0300

    Update ControllerVenda.java

--- comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/controllers/ControllerVenda.java
+++ comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/controllers/ControllerVenda.java
@@ -1,11 +1,1 @@
-package br.com.softwareGrup.comanda.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/venda")
-public class ControllerVenda {
-
-
-}
