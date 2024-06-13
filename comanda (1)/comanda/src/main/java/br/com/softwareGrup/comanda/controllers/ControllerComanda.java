9ec3e42f245860a5d424b570a1892296d87e644e
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2024-06-13 20:14:59 -0300

    Update ControllerComanda.java

--- comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/controllers/ControllerComanda.java
+++ comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/controllers/ControllerComanda.java
@@ -1,37 +1,88 @@
 package br.com.softwareGrup.comanda.controllers;
-
-import br.com.softwareGrup.comanda.model.ItemPedido;
 import br.com.softwareGrup.comanda.model.Comanda;
+import br.com.softwareGrup.comanda.model.ItemPedido;
 import br.com.softwareGrup.comanda.servicies.ComandaServicies;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
 
-@RestController
-@RequestMapping("/comanda")
-public class ControllerComanda {
+@Named
+@ViewScoped
+public class ControllerComanda implements Serializable {
 
-    @Autowired
-    ComandaServicies pedidoServicies;
+    @Inject
+    private ComandaServicies comandaServicies;
 
-    @PostMapping
-    public Comanda CriarComanda(@RequestParam Long idMesa){
-        return pedidoServicies.CriarComanda(idMesa);
+    private Long idMesa;
+    private Long idComanda;
+    private Long idProduto;
+    private int quantidade;
+    private List<ItemPedido> itensComanda;
+    private Comanda comanda;
+
+    @PostConstruct
+    public void init() {
+        // Inicializações necessárias
     }
 
-    @PutMapping
-    public Comanda AdicionarItemPedidoNaComanda(@RequestParam Long idComanda, @RequestParam Long idProduto, @RequestParam int quantidade){
-        return pedidoServicies.AdicionarItemPedido(idComanda, idProduto, quantidade);
+    public void criarComanda() {
+        comanda = comandaServicies.CriarComanda(idMesa);
     }
 
-    @GetMapping("/itens")
-    public List<ItemPedido> getItensComanda(@RequestParam Long idComanda){
-        return pedidoServicies.GetItensComanda(idComanda);
+    public void adicionarItemPedidoNaComanda() {
+        comanda = comandaServicies.AdicionarItemPedido(idComanda, idProduto, quantidade);
     }
 
-    @GetMapping
-    public Comanda GetPedido(Long idComanda){
-        return pedidoServicies.getComanda(idComanda);
+    public void carregarItensComanda() {
+        itensComanda = comandaServicies.GetItensComanda(idComanda);
+    }
+
+    public void carregarComanda() {
+        comanda = comandaServicies.getComanda(idComanda);
+    }
+
+    // Getters e Setters
+    public Long getIdMesa() {
+        return idMesa;
+    }
+
+    public void setIdMesa(Long idMesa) {
+        this.idMesa = idMesa;
+    }
+
+    public Long getIdComanda() {
+        return idComanda;
+    }
+
+    public void setIdComanda(Long idComanda) {
+        this.idComanda = idComanda;
+    }
+
+    public Long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public List<ItemPedido> getItensComanda() {
+        return itensComanda;
+    }
+
+    public Comanda getComanda() {
+        return comanda;
     }
 }
