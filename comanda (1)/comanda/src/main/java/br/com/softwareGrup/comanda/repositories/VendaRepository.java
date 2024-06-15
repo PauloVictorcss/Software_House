b8e5ca06e5eb07f6680d3ddf45222e30d0429c69
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2024-06-15 19:35:55 -0300

    Update VendaRepository.java

--- comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/repositories/VendaRepository.java
+++ comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/repositories/VendaRepository.java
@@ -2,13 +2,24 @@ package br.com.softwareGrup.comanda.repositories;
 
 import br.com.softwareGrup.comanda.model.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.Date;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface VendaRepository extends JpaRepository<Venda, Long> {
 
-    List<Venda> findByDataHoraPagamentoBetween(Date inicio, Date fim);
+    List<Venda> findByDataPagamentoBetween(LocalDate inicio, LocalDate fim);
 
+    @Query("SELECT p.nome AS produto, SUM(ip.quantidade) AS total_vendido " +
+            "FROM Venda v " +
+            "JOIN v.comanda c " +
+            "JOIN c.itens ip " +
+            "JOIN ip.produto p " +
+            "WHERE v.dataPagamento BETWEEN :dataInicio AND :dataFim " +
+            "GROUP BY p.nome " +
+            "ORDER BY total_vendido DESC")
+    List<Object[]> rankingProdutosMaisVendidos(LocalDate dataInicio, LocalDate dataFim);
 }
