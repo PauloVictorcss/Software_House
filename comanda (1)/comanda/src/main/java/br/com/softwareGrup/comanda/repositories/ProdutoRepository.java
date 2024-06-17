aa5950580d84ea3b6906ebc93aed315cdae99bb1
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2024-06-17 10:15:48 -0300

    Update ProdutoRepository.java

--- comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/repositories/ProdutoRepository.java
+++ comanda (1)/comanda/src/main/java/br/com/softwareGrup/comanda/repositories/ProdutoRepository.java
@@ -3,6 +3,10 @@ import br.com.softwareGrup.comanda.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    List<Produto> findByCategoria(String categoria);
 }
