fdbf9d0303b52e790605eb3d19899585fee68cbb
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2024-06-17 10:16:52 -0300

    Update V1__Create_Table_Mesa.sql

--- comanda (1)/comanda/src/main/resources/db.migration/V1__Create_Table_Mesa.sql
+++ comanda (1)/comanda/src/main/resources/db.migration/V1__Create_Table_Mesa.sql
@@ -14,27 +14,40 @@ CREATE TABLE `produto` (
   `descricao` varchar(255) DEFAULT NULL,
   `preco` decimal(10,2) NOT NULL,
   `categoria` varchar(80) DEFAULT NULL,
-  `imagem` varchar(255) DEFAULT NULL,
+  --`imagem` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `pedido` (
+CREATE TABLE `comanda` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `mesa_id` bigint NOT NULL,
   `data_hora` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_pedido_mesa` (`mesa_id`),
-  CONSTRAINT `fk_pedido_mesa` FOREIGN KEY (`mesa_id`) REFERENCES `mesa` (`id`)
+  KEY `fk_comanda_mesa` (`mesa_id`),
+  CONSTRAINT `fk_comanda_mesa` FOREIGN KEY (`mesa_id`) REFERENCES `mesa` (`id`)
 );
 
 CREATE TABLE `item_pedido` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `pedido_id` bigint NOT NULL,
+  `comanda_id` bigint NOT NULL,
   `produto_id` bigint NOT NULL,
   `quantidade` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_item_pedido_pedido` (`pedido_id`),
-  KEY `fk_item_pedido_produto` (`produto_id`),
-  CONSTRAINT `fk_item_pedido_pedido` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`),
-  CONSTRAINT `fk_item_pedido_produto` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`)
+  KEY `fk_item_comanda_comanda` (`pedido_id`),
+  KEY `fk_item_comanda_produto` (`produto_id`),
+  CONSTRAINT `fk_item_comanda_comanda` FOREIGN KEY (`comanda_id`) REFERENCES `comanda` (`id`),
+  CONSTRAINT `fk_item_comanda_produto` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`)
+);
+
+CREATE TABLE venda (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    mesa_id BIGINT,
+    comanda_id BIGINT,
+    forma_pagamento VARCHAR(255),
+    data_pagamento DATE,
+    valor_itens_pedidos DOUBLE,
+    desconto_porcentagem_garcon DOUBLE,
+    subtotal DOUBLE,
+    FOREIGN KEY (mesa_id) REFERENCES mesa(id),
+    FOREIGN KEY (comanda_id) REFERENCES comanda(id)
 );
